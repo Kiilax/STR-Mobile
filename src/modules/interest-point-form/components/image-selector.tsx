b191c75f33d07aa39ee0462c6a101650c9677465
simon <sihusser67@gmail.com>
2025-11-26 20:49:56 +0100

    fix(map): STR-102 fixed the trace refresh

--- src/modules/interest-point-form/components/image-selector.tsx
+++ src/modules/interest-point-form/components/image-selector.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from "react-native"
 import { Ionicons } from "@expo/vector-icons"
 import { colors } from "@/constants/theme"
-import { useImageSelector } from "@/hooks"
+import { useImageSelector } from "../hooks"
 
 interface ImageSelectorProps {
   selectedImages: string[]
