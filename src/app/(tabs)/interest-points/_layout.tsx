00e981bcc2f9fae642a8ce1face83ffc1d7113eb
simon <sihusser67@gmail.com>
2025-11-27 11:58:34 +0100

    fix(nav): STR-102 fixed warning with _layout

--- src/app/(tabs)/interest-points/_layout.tsx
+++ src/app/(tabs)/interest-points/_layout.tsx
@@ -6,7 +6,7 @@ export default function InterestPointsLayout() {
 
   return (
     <Stack screenOptions={screenOptions}>
-      <Stack.Screen name="interest-points" options={{ title: "Points d'intérêt" }} />
+      <Stack.Screen name="index" options={{ title: "Points d'intérêt" }} />
       <Stack.Screen name="[id]" options={{ title: "Détail du point d'intérêt" }} />
     </Stack>
   )
