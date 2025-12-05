a4f5067770e3d8d561b94d8713e8990445e1b87b
Roman NITZSCHE <roman.nitzsche67@gmail.com>
2025-12-05 08:12:37 +0100

    fix(apk): fixed apk url generated by eas

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ JSON=$(eas build --platform android --profile apk --non-interactive --json) || {
 BUILD_ID=$(echo "$JSON" | jq -r '.id // .buildId')
 
 RESULT=$(eas build:view "$BUILD_ID" --json)
-ARTIFACT_URL=$(echo "$RESULT" | jq -r '.artifacts[0].url // .artifactUrl')
+ARTIFACT_URL=$(echo "$RESULT" | jq -r '.artifacts.buildUrl // .artifacts.applicationArchiveUrl // empty')
 
 if [ -z "$ARTIFACT_URL" ]; then
   echo "No artifact URL found:"
