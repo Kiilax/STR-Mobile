b37808fa58daf367a8599b3ea98b1c9a46036be0
Roman NITZSCHE <roman.nitzsche67@gmail.com>
2025-12-05 08:12:37 +0100

    fix(apk): fixed apk url generated by eas
    wip(apk): test download of artifact

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-JSON=$(eas build --platform android --profile apk --non-interactive --json) || {
-  echo "eas build failed"
-  echo "$JSON"
-  exit 1
-}
+# JSON=$(eas build --platform android --profile apk --non-interactive --json) || {
+#   echo "eas build failed"
+#   echo "$JSON"
+#   exit 1
+# }
 
-BUILD_ID=$(echo "$JSON" | jq -r '.id // .buildId')
+# BUILD_ID=$(echo "$JSON" | jq -r '.id // .buildId')
 
-RESULT=$(eas build:view "$BUILD_ID" --json)
-ARTIFACT_URL=$(echo "$RESULT" | jq -r '.artifacts[0].url // .artifactUrl')
+# RESULT=$(eas build:view "$BUILD_ID" --json)
+# ARTIFACT_URL=$(echo "$RESULT" | jq -r '.artifacts.buildUrl // .artifacts.applicationArchiveUrl // empty')
 
-if [ -z "$ARTIFACT_URL" ]; then
-  echo "No artifact URL found:"
-  echo "$RESULT"
-  exit 1
-fi
+# if [ -z "$ARTIFACT_URL" ]; then
+#   echo "No artifact URL found:"
+#   echo "$RESULT"
+#   exit 1
+# fi
+
+$ARTIFACT_URL="https://expo.dev/artifacts/eas/mbAtaEwYYCgoSJ5DGBkdjn.apk"
 
 echo "Downloading APK from: $ARTIFACT_URL"
-mkdir -p output
-curl -L -o output/app.apk "$ARTIFACT_URL"
+mkdir -p /output
+curl -L -o /output/app.apk "$ARTIFACT_URL"
